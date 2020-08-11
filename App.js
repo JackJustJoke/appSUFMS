0f0101482ce23fe5e46eba5a1fb9166371e77983
Haksorus <gennadiy_sidorov_01@mail.ru>
2020-08-11 09:42:49 +0300

    Добавил ХУК

--- App.js
+++ App.js
@@ -1,14 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+//import { StatusBar } from 'expo-status-bar';
+import {React,useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Home, Calculator, WebSite, Article} from './src/screens'
+
+
 
 export default function App() {
+
+  const [{status},SetStatus]=useState("0")
+  
+  let screen = <Home></Home>
+
+  switch (status)
+  {
+  case "0": 
+   screen = <Home></Home>
+   case "1":
+   screen = <Calculator></Calculator>
+   case "2": 
+   screen = <WebSite></WebSite>
+   case "3":
+   screen = <Article></Article>
+  default:
+   screen = <Home></Home>
+  } 
+
+
+
+
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 const styles = StyleSheet.create({
---
+++ src/screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import View from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
